5429d62d48aea39d5a3bc2a04f86c3f62fea1ea3
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-06-28 23:44:39 +0100

    test change

--- alx_backend_graphql_crm/schema.py
+++ alx_backend_graphql_crm/schema.py
@@ -1,4 +1,7 @@
 import graphene
+import graphene
+from crm.schema import Query as CRMQuery, Mutation as CRMMutation
+
 
 class Query(graphene.ObjectType):
     hello = graphene.String()
@@ -6,4 +9,15 @@ class Query(graphene.ObjectType):
     def resolve_hello(self, info):
         return "Hello, GraphQL!"
 
-schema = graphene.Schema(query=Query)
+
+class Query(CRMQuery, graphene.ObjectType):
+    pass
+
+
+class Mutation(CRMMutation, graphene.ObjectType):
+    pass
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
+
+# schema = graphene.Schema(query=Query)
