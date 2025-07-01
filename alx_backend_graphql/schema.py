13a2fd589fda60ffb73f30a6de0b7df22383a0db
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-07-01 20:05:46 +0100

    Check if a class named Query exists in schema.py and inherits the graphene.ObjectType

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,6 +1,7 @@
 import graphene
+from crm.schema import CRMQuery  # Import your app's Query class
 
-class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello, GraphQL!")
+class Query(CRMQuery, graphene.ObjectType):
+    pass
 
 schema = graphene.Schema(query=Query)
