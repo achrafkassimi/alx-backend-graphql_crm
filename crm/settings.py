66b434e1bda162c17a1567311f38e20a925a0282
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-07-13 17:17:38 +0100

    crm/settings.py ??

--- crm/settings.py
+++ crm/settings.py
@@ -134,3 +134,9 @@ CRONJOBS = [
     ('0 */12 * * *', 'crm.cron.update_low_stock'),
 ]
 
+CELERY_BEAT_SCHEDULE = {
+    'generate-crm-report': {
+        'task': 'crm.tasks.generate_crm_report',
+        'schedule': crontab(day_of_week='mon', hour=6, minute=0),
+    },
+}
